10e33222fca3be8e1226a67d6345e29d675cf4b7
Carlos Augusto Mendes <52588762+carlosmendesrv@users.noreply.github.com>
2020-06-12 20:09:17 -0300

    Add grid category - NLW (3-Day)

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -1,18 +1,17 @@
-
-function populateUFs(){
+function populateUFs() {
   const ufSelect = document.querySelector("select[name=uf]")
   fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-  .then( res => res.json())
-  .then( states => {
-    for (const state of states){
-      ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
-    }
-  })
+    .then(res => res.json())
+    .then(states => {
+      for (const state of states) {
+        ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+      }
+    })
 }
 
 populateUFs()
 
-function getCities(event){
+function getCities(event) {
   const citySelect = document.querySelector("[name=city]")
   const stateInput = document.querySelector("[name=state]")
 
@@ -22,22 +21,55 @@ function getCities(event){
   stateInput.value = event.target.options[indexOfSelectedState].text
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
-  
+
   citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
   citySelect.disabled = true
- 
+
   fetch(url)
-  .then( res => res.json())
-  .then( cities => {
+    .then(res => res.json())
+    .then(cities => {
 
-    for (const city of cities){
-      citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
-    }
+      for (const city of cities) {
+        citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+      }
 
-    citySelect.disabled = false
-  })
+      citySelect.disabled = false
+    })
 }
 
 document
-    .querySelector("select[name=uf]")
-    .addEventListener("change", getCities)
+  .querySelector("select[name=uf]")
+  .addEventListener("change", getCities)
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+  item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+let selectedItems = []
+
+function handleSelectedItem(event) {
+  const itemLi = event.target
+
+  itemLi.classList.toggle("selected")
+  const itemId = itemLi.dataset.id
+  // console.log(itemLi.dataset.id)
+  const alreadySelected = selectedItems.findIndex(item => {
+    const itemFound = item == itemId
+    return itemFound
+  })
+
+  if (alreadySelected >= 0) {
+    const filteredItems = selectedItems.filter(item => {
+      const itemIsDifferent = item != itemId
+      return itemIsDifferent
+    })
+
+    selectedItems = filteredItems
+  } else {
+      selectedItems.push(itemId)
+  }
+  collectedItems.value = selectedItems
+}
